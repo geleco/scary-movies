46bc8d65da8e1cb1c6d5fd1627673f215cc12159
geleco <rangel001@gmail.com>
2023-11-01 17:27:43 -0300

    integração com backend local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TestComponent from './Components/TestComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Projeto React</h1>
+            </header>
+            <main>
+                <TestComponent />
+            </main>
+        </div>
+    );
 }
 
 export default App;
