7021addedb7120ffd8746298da73a1ad41a69306
lsiin <vitorantonio-7@hotmail.com>
2023-11-06 18:42:42 -0300

    Atuallização

--- src/Components/RegisterPage.js
+++ src/Components/RegisterPage.js
@@ -1,20 +1,20 @@
 import React from "react";
-import './styles/LoginPage.css';
+import './styles/RegisterPage.css';
 
- function LoginPage () {
+
+ function RegisterPage () {
     return (
-<div class="Login_page">
+
+<div class="Register_page">
 <img src=""/>
 <div class="Login_box">
-<h1> Log in </h1>
+<h1> Cadastre-se </h1>
 <P>Usuário<input type="text" placeholder="usuário"/> </P>
 <P>Senha<input type="password" placeholder="senha"/></P>
-<h4>cadastre-se</h4>
-<button>Entrar</button>
+<button>Cadastre-se</button>
 </div>
-
 </div>
     );
 
  }
- export default LoginPage
+ export default RegisterPage
