2b2f6a8b2b148533dc81b4c2de105dedf0e960db
lsiin <vitorantonio-7@hotmail.com>
2023-11-06 20:33:50 -0300

    o

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 import HomePage from './Components/HomePage';
-import LoginPage from './Components/LoginPage'
-import RegisterPage from './Components/RegisterPage'
 import Header from './Components/Header';
 
 function App() {
