dd17b7be678e278c7cee14cef02c2b3c9bb1d3ad
lsiin <vitorantonio-7@hotmail.com>
2023-11-06 20:33:36 -0300

    fxc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import HomePage from './Components/HomePage';
+import LoginPage from './Components/LoginPage'
+import RegisterPage from './Components/RegisterPage'
 import Header from './Components/Header';
 
 function App() {
