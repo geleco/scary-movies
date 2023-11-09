ef15e78c0cc3963887e704e6882cc61dcb971664
igorchl <quimerafiresao@gmail.com>
2023-11-08 22:38:32 -0300

    adição ao info

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Header from './Header'; // Você precisa adicionar o caminho correto para o seu componente Header aqui
 import './styles/HomePage.css';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
     return (
         <div>
             <Header hideSearchBar={false} />
             {/* ... resto do seu HomePage ... */}
-        </div>
+            <div>
+            <Link to="/info">
+              <button type="submit" className="button_login">Informação</button>
+            </Link>
+            </div>
+        </div> 
+
     );
 }
 
---
+++ src/Components/InfoPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const FilmeInfo = ({ filme }) => {
+  return (
+    <div>
+          Assistir ao trailer
+    </div>
+  );
+};
+
+export default FilmeInfo;
