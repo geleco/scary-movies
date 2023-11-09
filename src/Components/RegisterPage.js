4fde6427495efb8d9606e9c65c5e6e682933ddc2
lsiin <vitorantonio-7@hotmail.com>
2023-11-08 22:31:22 -0300

    confirmar senha

--- src/Components/RegisterPage.js
+++ src/Components/RegisterPage.js
@@ -3,45 +3,62 @@ import './styles/RegisterPage.css';
 import logo from './assets/logo.png';
 import { Link } from "react-router-dom";
 import Header from "./Header";
+import { auth } from './firebase.js'; 
 
 function RegisterPage() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState(""); // Adicione o estado para rastrear mensagens de erro
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
+
+    if (password !== confirmPassword) {
+      setError("As senhas não coincidem. Por favor, digite novamente.");
+      return;
+    }
+
+    // Registrar um novo usuário com email e senha
+    auth.createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // O usuário foi registrado com sucesso, você pode acessar userCredential.user
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        setError(error.message); // Tratar erros de registro
+      });
   };
 
   return (
-    <><div>
+    <div>
       <Header hideSearchBar={true} showLogout={false} />
-    </div>
-
-    <div className="Register_page">
+      <div className="Register_page">
         <img src={logo} alt="Logo" className="Background_logo" />
         <div className="Register_box">
-
           <form onSubmit={handleSubmit}>
             <div className="form-group">
-              <input className="input_usuario" type="text" placeholder="     Usuário" value={username} onChange={(e) => setUsername(e.target.value)} />
+              <input className="input_usuario" type="text" placeholder="Usuário" value={username} onChange={(e) => setUsername(e.target.value)} />
             </div>
             <div className="form-group">
-              <input className="input-mail" type="text" placeholder="     E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <input className="input-mail" type="text" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className="form-group">
-              <input className="input_senha" type="password" placeholder="     Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
+              <input className="input_senha" type="password" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
             <div className="form-group">
-              <input className="input_senha" type="password" placeholder="     Confirmar senha" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+              <input className="input_senha" type="password" placeholder="Confirmar senha" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
             </div>
+            {error && <p className="error-message">{error}</p>} {/* Exibe a mensagem de erro, se houver */}
             <button className="Button_Register" type="submit">Cadastre-se</button>
             <span className="span_register">Já tem uma conta?<Link to="/login">Faça o login</Link></span>
           </form>
         </div>
-      </div></>
+      </div>
+    </div>
   );
 }
 
 export default RegisterPage;
+
--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyC4cdUypn_cHxPtFBTXYpUNMHtcpQwYgTs",
@@ -15,7 +13,9 @@ const firebaseConfig = {
   measurementId: "G-RLFPL2L4NS"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig); 
+
+const auth = getAuth(app); 
 const analytics = getAnalytics(app);
-export { auth, firestore };
+
+export { auth, analytics }; 
