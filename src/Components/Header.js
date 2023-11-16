0c9836dc107eaa6b68b902522880cf592462e207
lsiin <vitorantonio-7@hotmail.com>
2023-11-16 20:22:18 -0300

    wdq

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,21 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import logo from "./assets/logo.png";
 import "./styles/Header.css";
 import { Link, useNavigate } from "react-router-dom";
-import FontSizeContext from './FontSizeContext'; // Importe o FontSizeContext
 
 function Header({ hideSearchBar, showLogout = true, hideNavLinks = false }) {
   const [searchTerm, setSearchTerm] = useState('');
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [tamanhoFonte, setTamanhoFonte] = useState(1);
   const navigate = useNavigate();
-  const { increaseFontSize } = useContext(FontSizeContext); // Utilize o contexto de tamanho da fonte
+
+  const aumentarFonte = () => {
+    setTamanhoFonte(tamanhoFonte * 3 );
+  };
+
+  const resetarFonte = () => {
+    setTamanhoFonte(1);
+  };
 
   const handleLogout = () => {
     console.log("Logout clicked");
@@ -19,15 +26,19 @@ function Header({ hideSearchBar, showLogout = true, hideNavLinks = false }) {
     navigate(`/home?search=${searchTerm}`);
   };
 
+  const estilo = {
+    "--tamanho-fonte": tamanhoFonte,
+  };
+
   return (
-    <header className="header">
+    <header className="header" style={estilo}>
       <div className="logo-container">
         <img src={logo} alt="Logo" className="header-logo" />
         <div className="menu-icon" onClick={() => setIsMenuOpen(!isMenuOpen)}>
           ☰
         </div>
       </div>
-      
+
       {!hideNavLinks && (
         <nav className={isMenuOpen ? 'nav-active' : ''}>
           <Link to="/home" className="nav-link">Home</Link>
@@ -49,6 +60,11 @@ function Header({ hideSearchBar, showLogout = true, hideNavLinks = false }) {
         </form>
       </div>
 
+      <div>
+        <button onClick={aumentarFonte}>Aumentar Fonte</button>
+        <button onClick={resetarFonte}>Redefinir Fonte</button>
+      </div>
+
       {showLogout && (
         <div className="logout-container">
           <Link to='/'>
@@ -58,9 +74,6 @@ function Header({ hideSearchBar, showLogout = true, hideNavLinks = false }) {
           </Link>
         </div>
       )}
-
-      {/* Botão para aumentar o tamanho da fonte */}
-      <button onClick={increaseFontSize}>Aumentar Fonte</button>
     </header>
   );
 }
